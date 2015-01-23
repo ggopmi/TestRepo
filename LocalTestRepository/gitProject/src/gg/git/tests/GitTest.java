3d6ae32db558b20fd2c8a55b263bd0ae2b76c969
Gregory Gleyzer <ggopmi@yahoo.com>
2015-01-22 20:57:57 -0500

    First Change

--- LocalTestRepository/gitProject/src/gg/git/tests/GitTest.java
+++ LocalTestRepository/gitProject/src/gg/git/tests/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
 		System.out.println(s1==s2);
 		System.out.println(s1.substring(1)==s2.substring(1));
 		System.out.println(s1.substring(1).intern()==s2.substring(1).intern());
+		
+		System.out.println(s1.substring(1)==s1.substring(1));
 	}
 }
